188138b14610c2a6ea21433b37b58920867394a5
Jeong-wan <gka3323@gmail.com>
2022-04-24 00:00:18 +0900

    post

---
+++ src/main/java/com/prac/good/service/posts/PostsService.java
@@ -0,0 +1,29 @@
+package com.prac.good.service.posts;
+import com.prac.good.domain.posts.PostsRepository;
+import com.prac.good.web.dto.PostsResponseDto;
+import com.prac.good.web.dto.PostsSaveRequestDto;
+import com.prac.good.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.prac.good.domain.posts.Posts;
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
+}
